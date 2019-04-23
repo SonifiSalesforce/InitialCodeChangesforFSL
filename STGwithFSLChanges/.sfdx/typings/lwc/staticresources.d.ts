2809089821ba98acb249a16bed5ffaab997b1188
Joan Jackson <jjackson@sonifi.com>
2019-04-23 13:42:04 -0500

    Add git nature to code files.

---
+++ STGwithFSLChanges/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,20 @@
+declare module "@salesforce/resourceUrl/CCSPTPArchive" {
+    var CCSPTPArchive: string;
+    export default CCSPTPArchive;
+}
+declare module "@salesforce/resourceUrl/CustomResources" {
+    var CustomResources: string;
+    export default CustomResources;
+}
+declare module "@salesforce/resourceUrl/ForceUI" {
+    var ForceUI: string;
+    export default ForceUI;
+}
+declare module "@salesforce/resourceUrl/SF_logo" {
+    var SF_logo: string;
+    export default SF_logo;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
